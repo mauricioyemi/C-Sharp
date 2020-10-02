3b33afa455ed734005d2f80bbe9a7119c8bfebc3
mauricioyemi <72272047+mauricioyemi@users.noreply.github.com>
2020-10-02 14:10:01 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace C__Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // calcular quantidade de gasolina necessária para viagem
+
+            int kilomLitro,disPercorrida,litrosComb;
+            Console.WriteLine("Que distância você vai viajar?");
+            disPercorrida = int.Parse(Console.ReadLine());
+            kilomLitro = 12;
+            litrosComb = disPercorrida / kilomLitro;
+            
+            Console.WriteLine("Você vai precisar de {0} litros de gasolina",litrosComb);
+        }
+    }
+}
