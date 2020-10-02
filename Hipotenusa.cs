0ac866bd18a4b662fad184b6e362518a8aafb61c
mauricioyemi <72272047+mauricioyemi@users.noreply.github.com>
2020-10-02 18:07:03 -0300

    Add files via upload

---
+++ Hipotenusa.cs
@@ -0,0 +1,29 @@
+﻿// Programa em C# para calcular a hipotenusa a partir dos valores dos catetos
+
+using System;
+namespace teste
+{
+class Program
+{
+static void Main(string[] args)
+{
+double cateto1, cateto2, hipotenusa;
+string continua;
+do{
+Console.Write("Informe cateto oposto:");
+cateto1 = double.Parse(Console.ReadLine());
+Console.Write("Informe cateto adjacente: ");
+cateto2 = double.Parse(Console.ReadLine());
+hipotenusa = Math.Sqrt(Quadrado(cateto1) + Quadrado(cateto2));
+Console.WriteLine("Hipotenusa: " + hipotenusa);
+Console.Write("Deseja continuar? [s/n] ");
+continua = Console.ReadLine();
+}while(continua != "n");
+}
+static double Quadrado(double numero)
+{
+double quadrado = numero * numero;
+return quadrado;
+}
+}
+}
