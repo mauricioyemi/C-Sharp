f15e045ea3225879cb2dd20bd25387b91bcdc0e6
Mauricio Yemi Martins <72272047+mauricioyemi@users.noreply.github.com>
2020-10-02 23:39:57 -0300

    Add files via upload

---
+++ CelsiusFahrenheit.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * C# Program to Perform Celsius to Fahrenheit Conversion
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ 
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int celsius, faren;
+            Console.Write("Enter the Temperature in Celsius(°C) : ");
+            celsius = int.Parse(Console.ReadLine());
+            faren = (celsius * 9) / 5 + 32;
+            Console.WriteLine("This Temperature in Fahrenheit is(°F) : " + faren);
+            Console.ReadLine();
+ 
+        }
+    }
+}
