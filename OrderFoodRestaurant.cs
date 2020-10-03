46dd157754d4b1c8315d775066578837bff1e2bf
Mauricio Yemi Martins <72272047+mauricioyemi@users.noreply.github.com>
2020-10-03 18:13:05 -0300

    Add files via upload

---
+++ OrderFoodRestaurant.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+
+class Pedido{
+    static void Main(){
+        
+        string descricao;
+        //double valor_unitario;
+        //int quantidade;
+
+        List<string> pedidos = new List<string>();
+        
+        Console.WriteLine("Qual o seu pedido? ");
+        descricao = Console.ReadLine();
+        pedidos.Add(descricao);
+
+        while (!String.IsNullOrEmpty(descricao))
+        {
+            Console.WriteLine("Qual o seu pedido? ");
+            descricao = Console.ReadLine();
+            pedidos.Add(descricao);
+
+        }
+
+        foreach (object o in pedidos)
+        {
+            Console.WriteLine(o);
+        }
+
+    }
+}
