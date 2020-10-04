315d524ad88277d9e751ca9980514e3db4c1866f
Mauricio Yemi Martins <72272047+mauricioyemi@users.noreply.github.com>
2020-10-04 02:33:44 -0300

    Update OrderFoodRestaurant.cs

--- OrderFoodRestaurant.cs
+++ OrderFoodRestaurant.cs
@@ -1,55 +1,26 @@
 using System;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections;
 using System.Collections.Generic;
 
+class ItemPedido{
+    string descricao;
+    double valor_unitario;
+    int quantidade;
+}
+
 class Pedido{
-    static void Main(){
+
+   static void Main(){
+
+       List<string> Pedidos = new List<string>();
+       Pedidos.Add("Xis");
+       Pedidos.Add("Dez pila");
+       Pedidos.Add("Dois");
+
+       foreach (var pedido in Pedidos)
+       {
+           Console.WriteLine(pedido);
+       }
+   
         
-        string descricao="";
-        string valor_unitario=""; // declarar valor_unitario como double
-        string quantidade=""; // declarar quantidade como int
-
-        List<string> pedidos = new List<string>();
-        
-        // Como criar uma lista para guardar os valores de valor_unitario?
-        
-        Console.Write("\nQual o seu pedido? ");
-        descricao = Console.ReadLine();
-        pedidos.Add(descricao);
-
-        Console.Write("\nQual o valor? ");
-        valor_unitario = Console.ReadLine();
-        pedidos.Add(valor_unitario); 
-
-        Console.Write("\nQuantidade? ");
-        quantidade = Console.ReadLine();
-        pedidos.Add(quantidade);
-
-        while (!String.IsNullOrEmpty(descricao))
-        {
-            Console.Write("\nQual o seu pedido? ");
-            descricao = Console.ReadLine();
-            pedidos.Add(descricao);
-
-            Console.Write("\nQual o valor? ");
-            valor_unitario = Console.ReadLine();
-            pedidos.Add(valor_unitario);
-
-            Console.Write("\nQuantidade? ");
-            quantidade = Console.ReadLine();
-            pedidos.Add(quantidade);
-
-        }
-
-        Console.WriteLine("\nConfirme seu pedido:\n");
-
-        foreach (object o in pedidos)
-        {
-            Console.WriteLine("\n" + o); // Preciso melhorar a formatação da saída na tela.
-        }
-                // Como mostrar o valor total do pedido?
     }
 }
